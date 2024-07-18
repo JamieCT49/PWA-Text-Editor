aff671c4acfacd08392a22c2f3a5c85d9d2ec1e0
James <jamescamerontaylor20@gmail.com>
2024-07-18 18:44:49 +1000

    fixed service worker

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,15 +26,6 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 // TODO: Implement asset caching
-registerRoute(
-  ({request}) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
-    cacheName: 'assets', 
-    plugins: [new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-  ],
-}),
-);
+registerRoute(({request}) => request.destination === 'document', pageCache);
 
-registerRoute();
+offlineFallback();
